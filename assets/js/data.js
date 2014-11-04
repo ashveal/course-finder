c8ebf484ae705b7763bf28347445d246082ad9d6
Nathan Wallis <nwallis@tinpotradio.com.au>
2014-11-04 13:51:26 +1100

    testing data

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,52 +1,61 @@
 data = [{
+        "id":1,
 		"uni":"Ballarat",
         "name": "Education",
         "description": "Nam dapibus mauris non libero blandit pellentesque. Fusce sed malesuada augue. Vivamus elit felis, pharetra at nisl eget, nisi.  Keywords: Helping, Academic"
     },
 
     {
+        "id":2,
     	"uni":"Ballarat",
         "name": "Commerce",
         "description": "Vivamus elit felis, pharetra at nisl eget, suscipit aliquet nulla. Pellentesque diam nisi.  Keywords: Commercial"
     },
 
     {
+        "id":3,
     	"uni":"Ballarat",
         "name": "Infotech",
         "description": "Duis quis ipsum turpis. Nam commodo, nisl at pretium bibendum. Keywords: Commercial"
     },
 
     {
+        "id":4,
     	"uni":"Horsham",
         "name": "Nursing",
         "description": "In bibendum aliquet nisl, sed egestas mi laoreet a. Maecenas ligula felis, venenatis vel euismod sit amet, sollicitudin eu diam. Praesent aliquet magna urna, et consequat mi lacinia id. Morbi interdum orci turpis, id dictum dui vulputate vel. Etiam facilisis eros quis nisl sagittis dignissim. Keywords: Helping, Academic"
     },
 
     {
+        "id":5,
     	"uni":"Horsham",
         "name": "Business",
         "description": "Mauris vitae nisi nisl. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed tempus pellentesque mi id tristique. Keywords: Commercial"
     },
 
     {
+        "id":6,
     	"uni":"Horsham",
         "name": "Mining",
         "description": "Maecenas vitae tortor eu justo blandit rhoncus. Donec consequat, ex ut lobortis rhoncus, nulla nibh cursus velit, ac accumsan est arcu sit amet lectus. Nulla mattis est quis fringilla ultrices. Keywords: Outdoors, Commercial"
     },
 
     {
+        "id":7,
     	"uni":"Horsham",
         "name": "Sports_science",
         "description": "Ut laoreet dapibus commodo. In luctus dui eu risus consectetur interdum ut ullamcorper tortor. Morbi tempor massa sed condimentum finibus. Phasellus ultrices magna a mauris lobortis fringilla. Etiam ut pretium ligula. Proin nec faucibus arcu. Keywords: Helping, Academic"
     },
 
     {
+        "id":8,
     	"uni":"Churchill",
         "name": "Physical_ed",
         "description": "Duis vehicula libero et suscipit pharetra. Cras sodales quis risus sit amet volutpat. Donec at dapibus dolor.  Keywords: Outdoors, Helping"
     },
 
     {
+        "id":9,
     	"uni":"Churchill",
         "name": "Science",
         "description": "Morbi interdum orci turpis, id dictum dui vulputate vel. Etiam facilisis eros quis nisl sagittis dignissim in vitae sem. Ut laoreet dapibus commodo. In luctus dui eu risus consectetur interdum ut ullamcorper tortor. Morbi tempor massa sed condimentum finibus. Phasellus ultrices magna a mauris lobortis fringilla. Etiam ut pretium ligula. Proin nec faucibus arcu. Keywords: Commercial, Academic"
