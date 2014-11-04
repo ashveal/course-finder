5a0a5f82ad1b8965b3be98060f212a6161e2a60f
Ben Sturmfels <ben@sturm.com.au>
2014-11-04 15:19:45 +1100

    Allow pages with different data.

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,20 +1,107 @@
-data = [
-{"name":"Education","description":"Nam dapibus mauris non libero blandit pellentesque. Fusce sed malesuada augue. Vivamus elit felis, pharetra at nisl eget, nisi.  Keywords: Helping, Academic"},
+var disciplines = [
+    {
+	'name': 'Automotive',
+	'color': '#005696',
+    },
+    {
+	'name': 'Building & Construction',
+	'color': '#007DC6',
+    },
+    {
+	'name': 'Cookery & Service Skills',
+	'color': '#005696',
+    },
+    {
+	'name': 'Hair & Beauty',
+	'color': '#00A1B2',
+    },
+    {
+	'name': 'Rural & Horticulture',
+	'color': '#7DC353',
+    },
+    {
+	'name': 'Engineering',
+	'color': '#62217F',
+    },
+    {
+	'name': 'Environmental, Physical & Mathematical Sciences',
+	'color': '#FDB813',
+    },
+    {
+	'name': 'Information Technology',
+	'color': '#E20969',
+    },
+    {
+	'name': 'Business & Sustainability',
+	'color': '#FFD204',
+    },
+    {
+	'name': 'Occupational Health & Safety',
+	'color': '#007DC6',
+    },
+    {
+	'name': 'Nursing, Midwifery & Healthcare',
+	'color': '#005696',
+    },
+    {
+	'name': 'General, Medical & Food Sciences',
+	'color': '#007DC6',
+    },
+    {
+	'name': 'Human Movement & Sports Sciences',
+	'color': '#005696',
+    },
+    {
+	'name': 'Education & Children\'s Services',
+	'color': '#00A1B2',
+    },
+    {
+	'name': 'Creative, Visual & Performing Arts',
+	'color': '#7DC353',
+    },
+    {
+	'name': 'Humanities & Social Science',
+	'color': '#62217F',
+    },
+    {
+	'name': 'Psychology & Criminal Justice',
+	'color': '#62217F',
+    },
+    {
+	'name': 'Research',
+	'color': '#62217F',
+    },
+];
 
-{"name":"Commerce","description":"Vivamus elit felis, pharetra at nisl eget, suscipit aliquet nulla. Pellentesque diam nisi.  Keywords: Commercial"},
-
-{"name":"Infotech","description":"Duis quis ipsum turpis. Nam commodo, nisl at pretium bibendum. Keywords: Commercial"},
-
-{"name":"Nursing","description":"In bibendum aliquet nisl, sed egestas mi laoreet a. Maecenas ligula felis, venenatis vel euismod sit amet, sollicitudin eu diam. Praesent aliquet magna urna, et consequat mi lacinia id. Morbi interdum orci turpis, id dictum dui vulputate vel. Etiam facilisis eros quis nisl sagittis dignissim. Keywords: Helping, Academic"},
-
-{"name":"Business","description":"Mauris vitae nisi nisl. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed tempus pellentesque mi id tristique. Keywords: Commercial"},
-
-{"name":"Mining","description":"Maecenas vitae tortor eu justo blandit rhoncus. Donec consequat, ex ut lobortis rhoncus, nulla nibh cursus velit, ac accumsan est arcu sit amet lectus. Nulla mattis est quis fringilla ultrices. Keywords: Outdoors, Commercial"},
-
-{"name":"Sports_science","description":"Ut laoreet dapibus commodo. In luctus dui eu risus consectetur interdum ut ullamcorper tortor. Morbi tempor massa sed condimentum finibus. Phasellus ultrices magna a mauris lobortis fringilla. Etiam ut pretium ligula. Proin nec faucibus arcu. Keywords: Helping, Academic"},
-
-{"name":"Physical_ed","description":"Duis vehicula libero et suscipit pharetra. Cras sodales quis risus sit amet volutpat. Donec at dapibus dolor.  Keywords: Outdoors, Helping"},
-
-{"name":"Science","description":"Morbi interdum orci turpis, id dictum dui vulputate vel. Etiam facilisis eros quis nisl sagittis dignissim in vitae sem. Ut laoreet dapibus commodo. In luctus dui eu risus consectetur interdum ut ullamcorper tortor. Morbi tempor massa sed condimentum finibus. Phasellus ultrices magna a mauris lobortis fringilla. Etiam ut pretium ligula. Proin nec faucibus arcu. Keywords: Commercial, Academic"}
-
-]
+var programs = [
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+    {
+	'name': 'Graduate Diploma of Outdoor & Environmental Education',
+    },
+];
